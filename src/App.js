a97ff6ce2b0be95bcc2959d5461f65125bd9a415
Ryan Dixon <r.dixon@reply.com>
2020-03-30 09:26:15 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,50 +1,104 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import React from 'react';
+import SidebarComponent from './sidebar/sidebar';
+import EditorComponent from './editor/editor';
+import './App.css';
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
+const firebase = require('firebase');
 
-  handleClick = api => e => {
-    e.preventDefault()
+class App extends React.Component {
 
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
+  constructor() {
+    super();
+    this.state = {
+      selectedNoteIndex: null,
+      selectedNote: null,
+      notes: null
+    };
   }
 
   render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
-}
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
+    return(
+      <div className="app-container">
+        <SidebarComponent 
+          selectedNoteIndex={this.state.selectedNoteIndex}
+          notes={this.state.notes}
+          deleteNote={this.deleteNote}
+          selectNote={this.selectNote}
+          newNote={this.newNote}></SidebarComponent>
+        {
+          this.state.selectedNote ?
+          <EditorComponent selectedNote={this.state.selectedNote}
+          selectedNoteIndex={this.state.selectedNoteIndex}
+          notes={this.state.notes}
+          noteUpdate={this.noteUpdate}></EditorComponent> :
+          null
+        }
       </div>
-    )
+    );
+  }
+
+  componentDidMount = () => {
+    firebase
+      .firestore()
+      .collection('notes')
+      .onSnapshot(serverUpdate => {
+        const notes = serverUpdate.docs.map(_doc => {
+          const data = _doc.data();
+          data['id'] = _doc.id;
+          return data;
+        });
+        console.log(notes);
+        this.setState({ notes: notes });
+      });
+  }
+
+  selectNote = (note, index) => this.setState({ selectedNoteIndex: index, selectedNote: note });
+  noteUpdate = (id, noteObj) => {
+    firebase
+      .firestore()
+      .collection('notes')
+      .doc(id)
+      .update({
+        title: noteObj.title,
+        body: noteObj.body,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp()
+      });
+  }
+  newNote = async (title) => {
+    const note = {
+      title: title,
+      body: ''
+    };
+    const newFromDB = await firebase
+      .firestore()
+      .collection('notes')
+      .add({
+        title: note.title,
+        body: note.body,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp()
+      });
+    const newID = newFromDB.id;
+    await this.setState({ notes: [...this.state.notes, note] });
+    const newNoteIndex = this.state.notes.indexOf(this.state.notes.filter(_note => _note.id === newID)[0]);
+    this.setState({ selectedNote: this.state.notes[newNoteIndex], selectedNoteIndex: newNoteIndex });
+  }
+  deleteNote = async (note) => {
+    const noteIndex = this.state.notes.indexOf(note);
+    await this.setState({ notes: this.state.notes.filter(_note => _note !== note) });
+    if(this.state.selectedNoteIndex === noteIndex) {
+      this.setState({ selectedNoteIndex: null, selectedNote: null });
+    } else {
+      this.state.notes.length > 1 ?
+      this.selectNote(this.state.notes[this.state.selectedNoteIndex - 1], this.state.selectedNoteIndex - 1) :
+      this.setState({ selectedNoteIndex: null, selectedNote: null });
+    }
+
+    firebase
+      .firestore()
+      .collection('notes')
+      .doc(note.id)
+      .delete();
   }
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -4,9 +4,22 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const firebase = require('firebase');
+require('firebase/firestore');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyB_Pq5AHrbKM2te-Ky1Zm45BqIodKLLNX0",
+  authDomain: "evernote-clone-44896.firebaseapp.com",
+  databaseURL: "https://evernote-clone-44896.firebaseio.com",
+  projectId: "evernote-clone-44896",
+  storageBucket: "evernote-clone-44896.appspot.com",
+  messagingSenderId: "915706065297",
+  appId: "1:915706065297:web:103bb9bcb967aef5"
+});
+
+ReactDOM.render(<App />, document.getElementById('evernote-container'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
